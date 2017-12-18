144c23000c7be9bdb3641b3e43b1c01def04c672
mdi-odoo <mdi@tinyerp.com>
2017-12-18 13:00:01 +0530

    [ADD] Added a mobile number for customer.

--- inuka/models/helpdesk.py
+++ inuka/models/helpdesk.py
@@ -9,3 +9,10 @@ class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     product_ids = fields.Many2many('product.product', 'helpdesk_ticket_product_rel', 'ticket_id', 'product_id', string="Products")
+    mobile = fields.Char("Customer Mobile")
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        super(HelpdeskTicket, self)._onchange_partner_id()
+        if self.partner_id:
+            self.mobile = self.partner_id.mobile
