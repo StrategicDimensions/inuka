f9d23a63b62977633bc9f9c5644776711d3f6415
mdi-odoo <mdi@tinyerp.com>
2017-12-18 16:27:00 +0530

    [ADD] Added a mechanism to create a sale order from helpdesk ticket.

--- inuka/models/helpdesk.py
+++ inuka/models/helpdesk.py
@@ -10,9 +10,47 @@ class HelpdeskTicket(models.Model):
 
     product_ids = fields.Many2many('product.product', 'helpdesk_ticket_product_rel', 'ticket_id', 'product_id', string="Products")
     mobile = fields.Char("Customer Mobile")
+    sale_order_count = fields.Integer(compute="_compute_sale_order_count", string="Sale Orders")
+
+    def _compute_sale_order_count(self):
+        SaleOrder = self.env['sale.order']
+        for ticket in self:
+            ticket.sale_order_count = SaleOrder.search_count([('ticket_id', '=', ticket.id)])
 
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
         super(HelpdeskTicket, self)._onchange_partner_id()
         if self.partner_id:
             self.mobile = self.partner_id.mobile
+
+    @api.multi
+    def create_sale_order(self):
+        self.ensure_one()
+        saleorder = self.env['sale.order'].create({
+            'partner_id': self.partner_id.id,
+            'ticket_id': self.id,
+        })
+        view_id = self.env.ref('sale.view_order_form').id
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'sale.order',
+            'views': [(view_id, 'form')],
+            'view_id': view_id,
+            'res_id': saleorder.id,
+        }
+
+    @api.multi
+    def view_sale_orders(self):
+        self.ensure_one()
+        orders = self.env['sale.order'].search([('ticket_id', '=', self.id)])
+        action = self.env.ref('sale.action_orders').read()[0]
+        action['domain'] = [('id', 'in', orders.ids)]
+        return action
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    ticket_id = fields.Many2one("helpdesk.ticket", string="Tickets")
