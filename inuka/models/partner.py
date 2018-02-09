d58dfd6039abc836d1fdf7b9d50b777d9e35819a
mdi-odoo <mdi@tinyerp.com>
2018-02-09 17:50:46 +0530

    [FIX] Fixed the error while calling method from xmlrpc.

--- inuka/models/partner.py
+++ inuka/models/partner.py
@@ -20,6 +20,19 @@ class Users(models.Model):
     def create(self, vals):
         return super(Users, self.with_context(from_user=True)).create(vals)
 
+    @api.model
+    def reset_password(self, login):
+        """ retrieve the user corresponding to login (login or email),
+            and reset their password
+        """
+        users = self.search([('login', '=', login)])
+        if not users:
+            users = self.search([('email', '=', login)])
+        if len(users) != 1:
+            raise Exception(_('Reset password: invalid username or email'))
+        return users.action_reset_password()
+
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
